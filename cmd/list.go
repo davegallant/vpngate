885f73db1c76e8ce7072d1fecc27ea9f5c3b99a5
Dave Gallant <dave.gallant@gmail.com>
2024-07-24 07:39:27 -0400

    Add proxy to list command also

--- cmd/list.go
+++ cmd/list.go
@@ -14,6 +14,8 @@ import (
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+	listCmd.Flags().StringVarP(&flagProxy, "proxy", "p", "", "provide a http/https proxy server to make requests through (i.e. http://127.0.0.1:8080)")
+	listCmd.Flags().StringVarP(&flagSocks5Proxy, "socks5", "s", "", "provide a socks5 proxy server to make requests through (i.e. 127.0.0.1:1080)")
 }
 
 var listCmd = &cobra.Command{
